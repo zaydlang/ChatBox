d9c90005066ceb7da0eeae03e5686d201ba77254
pancaikes <pranay.sen@gmail.com>
2017-10-03 14:11:09 -0700

    Add files via upload
    fuck you zayd

---
+++ EchoServer.java
@@ -0,0 +1,28 @@
+import java.net.*;
+import java.io.*;
+ 
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+         
+        int portNumber = 12345;
+         
+        try (
+            ServerSocket serverSocket =
+                new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();     
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);                   
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
