2e3a3afc4b7ce927fb6b27450b66db8ab28ed307
ZaydQumsieh <zayd@qumsieh.net>
2017-10-03 20:19:13 -0700

    created runnable class

---
+++ ForwardMessages.java
@@ -0,0 +1,41 @@
+import java.awt.event.*;
+import java.awt.*;
+
+import javax.swing.*;
+
+import java.net.*;
+
+import java.io.*;
+
+import java.util.*;
+
+// Thanks Horstmann!
+public class ForwardMessages implements Runnable {
+   private Socket s;
+   private Scanner in;
+   private PrintWriter out;
+   private ForwardMessages fm;
+   
+   public ForwardMessages(Socket client, ForwardMessages inputFM) {
+      fm = inputFM;
+      s  = client;
+   }
+   
+   public void run() {
+      try {
+         in  = new Scanner(s.getInputStream());
+         out = new PrintWriter(s.getOutputStream());
+         
+         displayMessage();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+   
+   public void displayMessage() {
+      while (in.hasNext()) {
+         String command = in.next();
+         System.out.println(command);
+      }
+   }
+}
