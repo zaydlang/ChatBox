460cf52e662f4b23b151894f392df6c2bc60d047
ZaydQumsieh <zayd@qumsieh.net>
2017-11-03 08:50:03 -0700

    Add files via upload

--- ihopethisworks/ChatboxInterface.java
+++ ihopethisworks/ChatboxInterface.java
@@ -9,6 +9,7 @@ import java.util.concurrent.*;
 import java.util.*;
 import java.net.*;
 import java.io.*;
+import javax.swing.JComponent.*;
 
 public class ChatboxInterface extends JFrame implements ActionListener {
    private final Font titleFont = new Font("Arial", Font.BOLD, 48);
@@ -18,14 +19,19 @@ public class ChatboxInterface extends JFrame implements ActionListener {
    private JTextField enterMessage;
    private JButton sendMessage;
    private PrintWriter out;
+   private String name;
 
-   public ChatboxInterface(String ip, int port, String[] args) throws Exception {
+   public ChatboxInterface(String ip, int port, String name, String[] args) throws Exception {
+      //setContentPane(new JLabel(new ImageIcon("images\\bckgrnd.jpg")));
+      Thread.sleep(10);
       Socket       s = new Socket(ip, port);
       InputStream  instream = s.getInputStream();
       OutputStream outstream = s.getOutputStream();
       Scanner      in = new Scanner(instream);
                    out = new PrintWriter(outstream); 
 
+      this.name = name;
+     
       out.println(args[0]);
       out.flush();
 
@@ -33,11 +39,16 @@ public class ChatboxInterface extends JFrame implements ActionListener {
       setLayout(null);
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
       
+      //JLabel test = new JLabel(new ImageIcon("images//bckgrnd.jpg"));
+      //test.setBounds(0, 0, 1300, 1100);
+      //setComponentZOrder(test, 0);
+      //add(test);
+          
       title = new JLabel("Chat with your friends!");
       title.setBounds(400, 25, 900, 100);
       title.setFont(titleFont);
       add(title);
-      
+
       chatHistory = new JTextArea();
       chatHistory.setBounds(50, 100 , 1175, 700);
       chatHistory.setEditable(false);
@@ -57,11 +68,12 @@ public class ChatboxInterface extends JFrame implements ActionListener {
       sendMessage.setBounds(1050, 825, 175, 175);
       sendMessage.addActionListener(this);
       add(sendMessage);
+      //chatHistory.setBackground(new Color(0, 0, 0, 100));
+      setVisible(true);
       
       this.getRootPane().setDefaultButton(sendMessage);
+      setLayout(null);
       
-      setVisible(true);
-
       while (true) {
          System.out.println(s.getInputStream().available());
          if (s.getInputStream().available() != 0) {
@@ -81,15 +93,17 @@ public class ChatboxInterface extends JFrame implements ActionListener {
    }
    
    public void appendMessage(String message) {
+      if (message.equals("")) return;
+      
       System.out.println("close");
       System.out.println(message);
       enterMessage.setText("");
       String messageHistory = chatHistory.getText();
-      chatHistory.setText(messageHistory + "\n" + "Pranay: " +  message + "\n");
+      chatHistory.setText(messageHistory + "\n" + name + ": " +  message + "\n");
    }
 
    public static void main(String[] args) throws Exception {
-      new ChatboxInterface("localhost", 34197, args);
+      new ChatboxInterface("localhost", 34197, "Test", args);
    }
 }
 
